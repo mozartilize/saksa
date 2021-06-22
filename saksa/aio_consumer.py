e6902a5afab27accb8d3f7f25a3e5fa7554090b2
mozart <mozartilize>
2021-06-22 23:17:37 +0700

    update AIOConsumer

--- saksa/aio_consumer.py
+++ saksa/aio_consumer.py
@@ -6,19 +6,27 @@ class AIOConsumer:
     def __init__(self, configs):
         self._consumer = Consumer(configs)
         self._cancelled = False
-        self._timeout = 10
 
-    def poll(self, send_to_trio):
-        msg = self._consumer.poll(self._timeout)
+    def _poll(self, send_to_trio, timeout):
+        msg = self._consumer.poll(timeout)
         trio.from_thread.run(send_to_trio.send, msg)
 
-    async def start(self):
+    def subscribe(self, topics):
+        self._consumer.subscribe(topics)
+
+    async def poll(self, timeout):
         send_to_trio, receive_from_thread = trio.open_memory_channel(0)
         async with trio.open_nursery() as nursery:
             while not self._cancelled:
-                nursery.start_soon(trio.from_thread.run_sync, self.poll, send_to_trio)
+                nursery.start_soon(trio.to_thread.run_sync, self._poll, send_to_trio, timeout)
                 msg = await receive_from_thread.receive()
-                print(msg)
+                if msg is None:
+                    continue
+                if msg.error():
+                    print("Consumer error: {}".format(msg.error()))
+                    continue
+
+                print('Received message: {}'.format(msg.value().decode('utf-8')))
 
     def close(self):
         self._cancelled = True
